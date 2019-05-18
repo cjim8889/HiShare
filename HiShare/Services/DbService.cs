28157a8b02b997d3244e513181934c0b789e89fd
Wuhao Chen <c.jim8889@outlook.com>
2019-05-18 23:12:54 +0100

    Add docker support

---
+++ HiShare/Services/DbService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiShare.Services
+{
+    public class DbService
+    {
+        public DbService(IConfiguration configuration)
+        {
+
+        }
+    }
+}
