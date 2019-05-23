8d6a198b4dd0ba0f90a29d9af6b50da63c933a2a
Wuhao Chen <43540548+cjim8889@users.noreply.github.com>
2019-05-23 01:30:30 +0100

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export Recaptcha_SecretKey="6LcW5KEUAAAAAJUlB1r539Z98-A5f-sNZbrLgE5t"
-export REACT_APP_API_URL="https://share.api.oxifus.com"
-export REACT_APP_RECAPTCHA_SITEKEY="6LcW5KEUAAAAALv-3CULoySYrCK1zKmZjOo0MbAM"
-
-docker-compose build
-docker-compose push
