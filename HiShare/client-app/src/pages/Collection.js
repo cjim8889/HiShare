fcc4cd1c4bf8b198a2f5a9f766363bb4101164ea
Wuhao Chen <c.jim8889@outlook.com>
2019-05-30 22:24:42 +0100

    link collection and collection edit:

--- HiShare/client-app/src/pages/Collection.js
+++ HiShare/client-app/src/pages/Collection.js
@@ -4,7 +4,15 @@ import Api from "../utilities/Api";
 import { Redirect } from "react-router-dom";
 import { Text } from 'office-ui-fabric-react/lib/Text';
 import CollectionList from "../components/CollectionList";
-import { IconButton } from 'office-ui-fabric-react';
+import {
+    DefaultButton,
+    Dialog,
+    DialogFooter,
+    DialogType,
+    IconButton,
+    PrimaryButton,
+    TextField
+} from 'office-ui-fabric-react';
 
 
 export default function Collection(props) {
@@ -28,11 +36,68 @@ export default function Collection(props) {
                 }
             </Text>
             <CollectionList articles={collection.articles}/>
-            <IconButton iconProps={{ iconName: 'Add' }} title="Edit"/>
+            <ToEditPage accessToken={collection.accessToken}/>
             {
                 invalidToken ?
                     <Redirect to="/404"/>
                     : null
+            }
+        </div>
+    )
+}
+
+function ToEditPage(props) {
+    const [isHidden, setIsHidden] = useState(true);
+    const [controlToken, setControlToken] = useState("");
+    const [disabled, setDisabled] = useState(true);
+    const [redirect, setRedirect] = useState(false);
+
+    function handleDismiss() {
+        setIsHidden(true);
+    }
+
+    function handleTextInput(e, value) {
+        if (value.trim().length > 0) {
+            setDisabled(false);
+        } else {
+            setDisabled(true);
+        }
+
+        setControlToken(value);
+    }
+
+    function handleSubmit() {
+        setRedirect(true);
+    }
+    function handleOpen() {
+        setIsHidden(false);
+    }
+
+    return (
+        <div>
+            <IconButton className="collection-button-edit" onClick={handleOpen} iconProps={{ iconName: 'Add' }} title="Edit"/>
+            <Dialog
+                onDismiss={handleDismiss}
+                hidden={isHidden}
+                dialogContentProps={{
+                    type: DialogType.normal,
+                    title: '编辑Collection',
+                }}
+                modalProps={{
+                    isBlocking: false,
+                    styles: { main: { maxWidth: 800 } }
+                }}
+            >
+                <TextField onChange={handleTextInput} label="Collection控制令牌 " required />
+                <DialogFooter>
+                    <PrimaryButton onClick={handleSubmit} disabled={disabled} text="确认" />
+                    <DefaultButton onClick={handleDismiss} text="取消" />
+                </DialogFooter>
+            </Dialog>
+            {
+                redirect ?
+                    <Redirect to={`/collections/${props.accessToken}/${controlToken}/edit`} />
+                :null
             }
         </div>
     )
