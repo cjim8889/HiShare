aa0b5a663f3d8591ba6f70f29bf8f5a7c83cd4d9
Wuhao Chen <c.jim8889@outlook.com>
2019-06-02 13:04:16 +0100

    remove redundant import

--- HiShare/client-app/src/components/PublishController.js
+++ HiShare/client-app/src/components/PublishController.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { CompoundButton } from 'office-ui-fabric-react/lib/Button';
 import { Icon } from 'office-ui-fabric-react/lib/Icon';
 import { TooltipHost } from 'office-ui-fabric-react/lib/Tooltip';
